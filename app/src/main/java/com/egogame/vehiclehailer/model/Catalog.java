12487c3f02f6772b560b7e4ac09eecfd41b77b3a
zzj198998 <284091793@qq.com>
2026-05-12 15:47:40 +0800

    添加id, getId(), getName()方法

--- app/src/main/java/com/egogame/vehiclehailer/model/Catalog.java
+++ app/src/main/java/com/egogame/vehiclehailer/model/Catalog.java
@@ -4,14 +4,19 @@ package com.egogame.vehiclehailer.model;
  * 目录分类
  */
 public class Catalog {
+    private static int nextId = 1;
+    private int id;
     private String catalog;
     private String displayName;
 
     public Catalog(String catalog, String displayName) {
+        this.id = nextId++;
         this.catalog = catalog;
         this.displayName = displayName;
     }
 
     public String getCatalog() { return catalog; }
     public String getDisplayName() { return displayName; }
+    public int getId() { return id; }
+    public String getName() { return getDisplayName(); }
 }
